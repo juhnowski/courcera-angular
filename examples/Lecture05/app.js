b9dddd3187a3c4af77380eb980217770c06e84ca
Juhnowski <juhnowski@gmail.com>
2019-09-22 16:03:02 +0300

    without additional requirements

--- examples/Lecture05/app.js
+++ examples/Lecture05/app.js
@@ -4,7 +4,7 @@
 angular.module('myFirstApp', [])
 
 .controller('MyFirstController', function ($scope) {
-  $scope.name = "Yaakov";
+  $scope.name = "Ilya";
   $scope.sayHello = function () {
     return "Hello Coursera!";
   };
